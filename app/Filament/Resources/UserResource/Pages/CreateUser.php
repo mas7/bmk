98c6bad7472a5a46e18f16854652c9547957c11f
Mohamed Sorour <mas.qa77@gmail.com>
2023-11-11 01:03:08 +0200

    BMK-11 - Send email to user after creation

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -3,10 +3,26 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Mail\UserRegistrationMail;
+use App\Models\User;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Mail;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        /** @var User $user */
+        $user = static::getModel()::create($data);
+
+        $user['plainPassword'] = $data['password'];
+
+        Mail::to($user->email)->send(new UserRegistrationMail($user));
+
+        return $user;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
+// Route::get('/email', function () {
+//     $data = (object)[
+//         'name' => 'Ahmed',
+//         'email' => 'ahmed@gmail.com',
+//         'phone_number' => '01020081375',
+//         'password' => 'ahmed@123',
+//         'role' => 2,
+//     ];
+//     return view('emails.user_registration', ['user' => $data]);
 // });
