e6572cc7dd591c37228daf77eb133f5ead95830b
Mohamed Sorour <mas.qa77@gmail.com>
2024-01-13 03:09:34 +0200

    BMK-33 - [x] Add tabs to tickets

--- app/Filament/Admin/Resources/TicketResource/Pages/ListTickets.php
+++ app/Filament/Admin/Resources/TicketResource/Pages/ListTickets.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Admin\Resources\TicketResource\Pages;
 
+use App\Enums\TicketStatus;
 use App\Filament\Admin\Resources\TicketResource;
+use App\Models\Ticket;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTickets extends ListRecords
 {
@@ -15,5 +19,38 @@ class ListTickets extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'ALL'       => Tab::make('All')
+                ->badge($this->getTableQuery()->count()),
+            'OPEN'      => Tab::make('Open')
+                ->badge($this->getTableQuery()->where('status', TicketStatus::OPEN->value)->count())
+                ->badgeColor('info')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', TicketStatus::OPEN->value)),
+            'ANSWERED'  => Tab::make('Answered')
+                ->badge($this->getTableQuery()->where('status', TicketStatus::ANSWERED->value)->count())
+                ->badgeColor('warning')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', TicketStatus::ANSWERED->value)),
+            'RESOLVED'  => Tab::make('Resolved')
+                ->badge($this->getTableQuery()->where('status', TicketStatus::RESOLVED->value)->count())
+                ->badgeColor('success')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', TicketStatus::RESOLVED->value)),
+            'POSTPONED' => Tab::make('Postponed')
+                ->badge($this->getTableQuery()->where('status', TicketStatus::POSTPONED->value)->count())
+                ->badgeColor('danger')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', TicketStatus::POSTPONED->value)),
+            'REVIEW'    => Tab::make('Review')
+                ->badge($this->getTableQuery()->where('status', TicketStatus::REVIEW->value)->count())
+                ->badgeColor('warning')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', TicketStatus::REVIEW->value)),
+        ];
+    }
+
+    public function getDefaultActiveTab(): string|int|null
+    {
+        return 'ALL';
     }
 }
--- app/Filament/Client/Resources/TicketResource/Pages/ListTickets.php
+++ app/Filament/Client/Resources/TicketResource/Pages/ListTickets.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Client\Resources\TicketResource\Pages;
 
+use App\Enums\TicketStatus;
 use App\Filament\Client\Resources\TicketResource;
 use App\Models\User;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -26,5 +28,38 @@ class ListTickets extends ListRecords
     protected function getTableQuery(): Builder
     {
         return parent::getTableQuery()->where('user_id', auth()->id())->withoutGlobalScopes();
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'ALL'       => Tab::make('All')
+                ->badge($this->getTableQuery()->count()),
+            'OPEN'      => Tab::make('Open')
+                ->badge($this->getTableQuery()->where('status', TicketStatus::OPEN->value)->count())
+                ->badgeColor('info')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', TicketStatus::OPEN->value)),
+            'ANSWERED'  => Tab::make('Answered')
+                ->badge($this->getTableQuery()->where('status', TicketStatus::ANSWERED->value)->count())
+                ->badgeColor('warning')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', TicketStatus::ANSWERED->value)),
+            'RESOLVED'  => Tab::make('Resolved')
+                ->badge($this->getTableQuery()->where('status', TicketStatus::RESOLVED->value)->count())
+                ->badgeColor('success')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', TicketStatus::RESOLVED->value)),
+            'POSTPONED' => Tab::make('Postponed')
+                ->badge($this->getTableQuery()->where('status', TicketStatus::POSTPONED->value)->count())
+                ->badgeColor('danger')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', TicketStatus::POSTPONED->value)),
+            'REVIEW'    => Tab::make('Review')
+                ->badge($this->getTableQuery()->where('status', TicketStatus::REVIEW->value)->count())
+                ->badgeColor('warning')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', TicketStatus::REVIEW->value)),
+        ];
+    }
+
+    public function getDefaultActiveTab(): string|int|null
+    {
+        return 'ALL';
     }
 }
